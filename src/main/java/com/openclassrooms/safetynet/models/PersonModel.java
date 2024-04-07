8287ab036bbd81fdf4985dd3e379f582a9cc6b1b
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-04-07 16:44:29 +0200

    forgot a @ToString on the PersonModel class

--- src/main/java/com/openclassrooms/safetynet/models/PersonModel.java
+++ src/main/java/com/openclassrooms/safetynet/models/PersonModel.java
@@ -3,10 +3,12 @@ package com.openclassrooms.safetynet.models;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@ToString
 public class PersonModel {
     public PersonModel(){
         //constructor for jackson serialisation
