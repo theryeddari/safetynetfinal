d9b4f9d2455b74425bc0ccda3734439c3252cf31
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-04-07 20:25:44 +0200

    modify parameter String to List<String>

--- src/main/java/com/openclassrooms/safetynet/dto/responses/PhoneAlertReplyPersonDTO.java
+++ src/main/java/com/openclassrooms/safetynet/dto/responses/PhoneAlertReplyPersonDTO.java
@@ -3,12 +3,11 @@ package com.openclassrooms.safetynet.dto.responses;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
-import lombok.Value;
-import org.springframework.core.type.filter.RegexPatternTypeFilter;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @ToString
 public class PhoneAlertReplyPersonDTO {
-    String phone;
+    List<String> phone;
 }
