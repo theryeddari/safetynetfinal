4ea80746ea1fe8300e5e51f1d071d2c64f05f47c
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-04-12 01:49:59 +0200

    modify Dto and add su-dto for Fire response

--- src/main/java/com/openclassrooms/safetynet/dto/responses/FireReplyPersonDTO.java
+++ src/main/java/com/openclassrooms/safetynet/dto/responses/FireReplyPersonDTO.java
@@ -1,19 +1,19 @@
 package com.openclassrooms.safetynet.dto.responses;
 
+import com.openclassrooms.safetynet.dto.responses.submodels.SubFireReplyReplyInfoPerson;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @ToString
 public class FireReplyPersonDTO {
-    String lastName;
-    String phone;
-    LocalDate birthdate;
-    List<String> medication;
-    List<String> allergies;
+    public FireReplyPersonDTO(){
+        //constructor for jackson serialisation
+    }
+    List<SubFireReplyReplyInfoPerson> infoPerson;
+    String stationNumber;
 }
