2e75fa7d9deeec0d8090162ab93f3edc0e79b5a0
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-10 19:09:30 +0200

    code test for updateFireStation

--- src/test/java/com/openclassrooms/safetynet/services/FireStationServiceTest.java
+++ src/test/java/com/openclassrooms/safetynet/services/FireStationServiceTest.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.safetynet.services;
 
-import com.openclassrooms.safetynet.dto.requests.AddOrUpdateFireStationDto;
+import com.openclassrooms.safetynet.dto.requests.AddFireStationDto;
+import com.openclassrooms.safetynet.dto.requests.UpdateFireStationDto;
 import com.openclassrooms.safetynet.models.FireStationModel;
 import com.openclassrooms.safetynet.utils.ManageJsonData;
 import org.junit.jupiter.api.Assertions;
@@ -14,8 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class FireStationServiceTest {
@@ -34,10 +34,25 @@ class FireStationServiceTest {
         when(manageJsonDataSpy.fireStationReaderJsonData()).thenReturn(listDataJsonFireStationForMock);
         //indicates not to launch the write method on the file
         doNothing().when(manageJsonDataSpy).fireStationWriterJsonData(anyList());
-        AddOrUpdateFireStationDto newFireStation = new AddOrUpdateFireStationDto("10 rue des frites","20");
+        AddFireStationDto newFireStation = new AddFireStationDto("10 rue des frites","20");
         fireStationService.addFireStation(newFireStation);
         Assertions.assertTrue(listDataJsonFireStationForMock.stream().anyMatch(fireStation ->
                 fireStation.getStation().equals(newFireStation.getStation())
                         && fireStation.getAddress().equals(newFireStation.getAddress())));
+    }
+    @Test
+    void updateFireStationTest() throws IOException {
+        UpdateFireStationDto updateFireStation = new UpdateFireStationDto("112 Steppes Pl","3","5");
+        List<FireStationModel> listUpdatedFireStation = new ArrayList<>();
+        doNothing().when(manageJsonDataSpy).fireStationWriterJsonData(anyList());
+        doAnswer(invocation -> {
+            listUpdatedFireStation.addAll(invocation.getArgument(0));
+            return null;
+        }).when(manageJsonDataSpy).fireStationWriterJsonData(anyList());
+        fireStationService.updateFireStation(updateFireStation);
+        Assertions.assertTrue(listUpdatedFireStation.stream().anyMatch(fireStation ->
+                fireStation.getStation().equals(updateFireStation.getNewNumberStation())
+                    && fireStation.getAddress().equals(updateFireStation.getAddress())
+        ));
     }
 }
