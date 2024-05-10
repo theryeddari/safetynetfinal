a4fb9e179ee26f8c791e058dee160b9547e15431
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-10 19:10:27 +0200

    code method to update fireStation

--- src/main/java/com/openclassrooms/safetynet/services/FireStationService.java
+++ src/main/java/com/openclassrooms/safetynet/services/FireStationService.java
@@ -1,12 +1,14 @@
 package com.openclassrooms.safetynet.services;
 
-import com.openclassrooms.safetynet.dto.requests.AddOrUpdateFireStationDto;
+import com.openclassrooms.safetynet.dto.requests.AddFireStationDto;
+import com.openclassrooms.safetynet.dto.requests.UpdateFireStationDto;
 import com.openclassrooms.safetynet.models.FireStationModel;
 import com.openclassrooms.safetynet.utils.ManageJsonData;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FireStationService {
@@ -17,11 +19,24 @@ public class FireStationService {
         this.manageJsonData = manageJsonData;
     }
 
-    public void addFireStation(AddOrUpdateFireStationDto newFireStation) throws IOException {
+    public void addFireStation(AddFireStationDto newFireStation) throws IOException {
         List<FireStationModel> listFireStationExisting = manageJsonData.fireStationReaderJsonData();
         if(listFireStationExisting.stream().noneMatch(fireStationExist -> fireStationExist.getStation().equals(newFireStation.getStation()) && fireStationExist.getAddress().equals(newFireStation.getAddress()))) {
             listFireStationExisting.add(new FireStationModel(newFireStation.getAddress(), newFireStation.getStation()));
         }
+        manageJsonData.fireStationWriterJsonData(listFireStationExisting);
+    }
+
+    public void updateFireStation(UpdateFireStationDto updateFireStation) throws IOException {
+        List<FireStationModel> listFireStationExisting = manageJsonData.fireStationReaderJsonData();
+        //get the reference to the filtered person object
+        Optional<FireStationModel> wantedFireStationUpdate = listFireStationExisting.stream().filter(fireStation ->
+                fireStation.getStation().equals(updateFireStation.getStation())
+                        && fireStation.getAddress().equals(updateFireStation.getAddress())).findFirst();
+        wantedFireStationUpdate.ifPresent(fireStation -> {
+            fireStation.setAddress(updateFireStation.getAddress());
+            fireStation.setStation(updateFireStation.getNewNumberStation());
+        });
         manageJsonData.fireStationWriterJsonData(listFireStationExisting);
     }
 }
