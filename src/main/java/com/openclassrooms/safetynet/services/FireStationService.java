c1b62ce264f66f3c039c26380f4797b1bab4a11e
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-10 21:47:34 +0200

    code method for delete Station request

--- src/main/java/com/openclassrooms/safetynet/services/FireStationService.java
+++ src/main/java/com/openclassrooms/safetynet/services/FireStationService.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.safetynet.services;
 
 import com.openclassrooms.safetynet.dto.requests.AddFireStationDto;
+import com.openclassrooms.safetynet.dto.requests.DeleteFireStationDto;
 import com.openclassrooms.safetynet.dto.requests.UpdateFireStationDto;
 import com.openclassrooms.safetynet.models.FireStationModel;
 import com.openclassrooms.safetynet.utils.ManageJsonData;
@@ -21,7 +22,7 @@ public class FireStationService {
 
     public void addFireStation(AddFireStationDto newFireStation) throws IOException {
         List<FireStationModel> listFireStationExisting = manageJsonData.fireStationReaderJsonData();
-        if(listFireStationExisting.stream().noneMatch(fireStationExist -> fireStationExist.getStation().equals(newFireStation.getStation()) && fireStationExist.getAddress().equals(newFireStation.getAddress()))) {
+        if (listFireStationExisting.stream().noneMatch(fireStationExist -> fireStationExist.getStation().equals(newFireStation.getStation()) && fireStationExist.getAddress().equals(newFireStation.getAddress()))) {
             listFireStationExisting.add(new FireStationModel(newFireStation.getAddress(), newFireStation.getStation()));
         }
         manageJsonData.fireStationWriterJsonData(listFireStationExisting);
@@ -37,6 +38,24 @@ public class FireStationService {
             fireStation.setAddress(updateFireStation.getAddress());
             fireStation.setStation(updateFireStation.getNewNumberStation());
         });
+        manageJsonData.fireStationWriterJsonData(listFireStationExisting);
+    }
+
+    public void deleteFireStation(DeleteFireStationDto deleteFireStation) throws IOException {
+        List<FireStationModel> listFireStationExisting = manageJsonData.fireStationReaderJsonData();
+        if (deleteFireStation.getStation() == null && deleteFireStation.getAddress() == null) {
+            //TODO: exception
+        } else {
+            if (deleteFireStation.getStation() == null ||deleteFireStation.getStation().isBlank()) {
+                listFireStationExisting.removeIf(fireStation -> fireStation.getAddress().equals(deleteFireStation.getAddress()));
+            } else if (deleteFireStation.getAddress() == null || deleteFireStation.getAddress().isBlank()) {
+                listFireStationExisting.removeIf(fireStation -> fireStation.getStation().equals(deleteFireStation.getStation()));
+            } else {
+                listFireStationExisting.removeIf(fireStation ->
+                        fireStation.getAddress().equals(deleteFireStation.getAddress())
+                                && fireStation.getStation().equals(deleteFireStation.getStation()));
+            }
+        }
         manageJsonData.fireStationWriterJsonData(listFireStationExisting);
     }
 }
