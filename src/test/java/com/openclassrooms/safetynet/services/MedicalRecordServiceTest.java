38a5c24e3599643c8071ca712bb29b98bc40a0e9
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-11 10:27:05 +0200

    adapt test of add person and update person at add and update medical record

---
+++ src/test/java/com/openclassrooms/safetynet/services/MedicalRecordServiceTest.java
@@ -0,0 +1,59 @@
+package com.openclassrooms.safetynet.services;
+
+import com.openclassrooms.safetynet.dto.requests.AddOrUpdateMedicalRecordDto;
+import com.openclassrooms.safetynet.models.MedicalRecordModel;
+import com.openclassrooms.safetynet.utils.ManageJsonData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class MedicalRecordServiceTest {
+    @Autowired
+    MedicalRecordService medicalRecordService;
+    @SpyBean
+    ManageJsonData manageJsonDataSpy;
+
+    MedicalRecordServiceTest(){
+    }
+    @Test
+    void addMedicalRecordTest() throws IOException {
+        //use of a modifiable table to store an initial list which can be dynamically modified by the method tested thanks to the reference
+        List<MedicalRecordModel> listDataJsonMedicalRecordForMock = new ArrayList<>(List.of(new MedicalRecordModel("Jean","Jacque","",List.of(""),List.of(""))));
+        //stub the mock method to introduce our list into the method
+        when(manageJsonDataSpy.medicalRecordReaderJsonData()).thenReturn(listDataJsonMedicalRecordForMock);
+        //indicates not to launch the write method on the file
+        doNothing().when(manageJsonDataSpy).medicalRecordWriterJsonData(anyList());
+        //person to add
+        AddOrUpdateMedicalRecordDto newMedicalRecord = new AddOrUpdateMedicalRecordDto("Jean","Jacque","04/13/2000",List.of("aspirin"),List.of("pollen"));
+        medicalRecordService.addMedicalRecord(newMedicalRecord);
+        Assertions.assertTrue(listDataJsonMedicalRecordForMock.stream().anyMatch(medicalRecord -> medicalRecord.getLastName().equals(newMedicalRecord.getLastName()) && medicalRecord.getFirstName().equals(newMedicalRecord.getFirstName())));
+    }
+    @Test
+    void updateMedicalRecordTest() throws IOException {
+        AddOrUpdateMedicalRecordDto updateMedicalRecord = new AddOrUpdateMedicalRecordDto("John","Boyd","",List.of(),List.of());
+        List<MedicalRecordModel> listUpdatedMedicalRecord = new ArrayList<>();
+        doNothing().when(manageJsonDataSpy).medicalRecordWriterJsonData(anyList());
+        doAnswer(invocation -> {
+            listUpdatedMedicalRecord.addAll(invocation.getArgument(0));
+            return null;
+        }).when(manageJsonDataSpy).medicalRecordWriterJsonData(anyList());
+        medicalRecordService.updateMedicalRecord(updateMedicalRecord);
+        Assertions.assertTrue(listUpdatedMedicalRecord.stream().anyMatch(medicalRecord ->
+                medicalRecord.getFirstName().equals(updateMedicalRecord.getFirstName())
+                        && medicalRecord.getLastName().equals(updateMedicalRecord.getLastName())
+                        && medicalRecord.getBirthdate().equals(updateMedicalRecord.getBirthdate())
+                        && medicalRecord.getMedications().equals(updateMedicalRecord.getMedications())
+                        && medicalRecord.getAllergies().equals(updateMedicalRecord.getAllergies())
+        ));
+    }
+}
