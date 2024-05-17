254c9dfc22f477c591f9d5af2586bbeed18f0802
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-17 11:53:47 +0200

    add log and refactoring vars name for explicit

--- src/main/java/com/openclassrooms/safetynet/utils/ManageJsonData.java
+++ src/main/java/com/openclassrooms/safetynet/utils/ManageJsonData.java
@@ -18,44 +18,66 @@ import java.util.Map;
 @Component
 public class ManageJsonData {
     ObjectMapper objectMapper;
+
     @Value("${path.file}")
     String path;
+
     File file;
+
     Map<String, Object> dataJson = new HashMap<>();
 
+    // Method called after constructing the object to initialize the file and read the JSON data, because Spring annotations are read last (avoid an empty path)
     @PostConstruct
     public void init() throws IOException {
-        this.file  = new File(path);
+        this.file = new File(path);
         globalReaderJsonData();
     }
 
+    // Constructor to inject the ObjectMapper
     public ManageJsonData(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
+
+    // Method to read global JSON data from file
     private void globalReaderJsonData() throws IOException {
-        this.dataJson = objectMapper.readValue(file, new TypeReference<>() {});
+        this.dataJson = objectMapper.readValue(file, new TypeReference<>() {
+        });
     }
 
+    // Method to read fire station data from JSON data
     public List<FireStationModel> fireStationReaderJsonData() {
-        return objectMapper.convertValue(dataJson.get("firestations"), new TypeReference<>() {});
+        return objectMapper.convertValue(dataJson.get("firestations"), new TypeReference<>() {
+        });
     }
-    public  List<PersonModel>  personReaderJsonData() {
-        return objectMapper.convertValue(dataJson.get("persons"), new TypeReference<>() {});
+
+    // Method to read people data from JSON data
+    public List<PersonModel> personReaderJsonData() {
+        return objectMapper.convertValue(dataJson.get("persons"), new TypeReference<>() {
+        });
     }
+
+    // Method to read medical records from JSON data
     public List<MedicalRecordModel> medicalRecordReaderJsonData() {
-        return objectMapper.convertValue(dataJson.get("medicalrecords"), new TypeReference<>() {});
+        return objectMapper.convertValue(dataJson.get("medicalrecords"), new TypeReference<>() {
+        });
     }
+
+    // Method to test/find a fire station by address
     public FireStationModel testFireStationReaderByReturn(String address) {
-       return fireStationReaderJsonData().stream().filter(listDTO -> listDTO.getAddress().equals(address))
-               .findFirst()
-                .orElse(null);
-    }
-    public PersonModel testPersonReaderByReturn(String firstName, String lastName) {
-        return personReaderJsonData().stream().filter(listDTO -> listDTO.getFirstName().equals(firstName)
-                && listDTO.getLastName().equals(lastName))
+        return fireStationReaderJsonData().stream().filter(listDTO -> listDTO.getAddress().equals(address))
                 .findFirst()
                 .orElse(null);
     }
+
+    // Method to test/find a person by first and last name
+    public PersonModel testPersonReaderByReturn(String firstName, String lastName) {
+        return personReaderJsonData().stream().filter(listDTO -> listDTO.getFirstName().equals(firstName)
+                        && listDTO.getLastName().equals(lastName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    // Method to test/find a medical record by first and last name
     public MedicalRecordModel testMedicalRecordReaderByReturn(String firstName, String lastName) {
         return medicalRecordReaderJsonData().stream().filter(listDTO -> listDTO.getFirstName().equals(firstName)
                         && listDTO.getLastName().equals(lastName))
@@ -63,18 +85,21 @@ public class ManageJsonData {
                 .orElse(null);
     }
 
-   public void personWriterJsonData(List<PersonModel> list) throws IOException {
+    // Method to write people data to the JSON file
+    public void personWriterJsonData(List<PersonModel> list) throws IOException {
         this.dataJson.replace("persons", list);
-        objectMapper.writer(new OutputFormatIndentationJsonData()).writeValue(file,dataJson);
+        objectMapper.writer(new OutputFormatIndentationJsonData()).writeValue(file, dataJson);
     }
 
+    // Method to write fire station data to the JSON file
     public void fireStationWriterJsonData(List<FireStationModel> list) throws IOException {
         this.dataJson.replace("firestations", list);
-        objectMapper.writer(new OutputFormatIndentationJsonData()).writeValue(file,dataJson);
+        objectMapper.writer(new OutputFormatIndentationJsonData()).writeValue(file, dataJson);
     }
 
+    // Method to write medical records to JSON file
     public void medicalRecordWriterJsonData(List<MedicalRecordModel> list) throws IOException {
         this.dataJson.replace("medicalrecords", list);
-        objectMapper.writer(new OutputFormatIndentationJsonData()).writeValue(file,dataJson);
+        objectMapper.writer(new OutputFormatIndentationJsonData()).writeValue(file, dataJson);
     }
 }
