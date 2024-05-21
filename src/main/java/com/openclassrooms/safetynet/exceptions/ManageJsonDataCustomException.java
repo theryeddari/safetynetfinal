d59cc7b8d840c6cc7d272d09280227e61506f52c
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-21 19:18:12 +0200

    code enclosing exceptions to each method who throwable (manageJSonData)

--- src/main/java/com/openclassrooms/safetynet/exceptions/ManageJsonDataCustomException.java
+++ src/main/java/com/openclassrooms/safetynet/exceptions/ManageJsonDataCustomException.java
@@ -8,13 +8,40 @@ public class ManageJsonDataCustomException extends Exception {
         super(message, cause);
     }
 
-    // Nested static class for person writer specific exceptions
-    public static class ManageJsonDataPersonWriterException extends ManageJsonDataCustomException {
+    // Nested static class for person writer method to encapsulate throwable general Exception in specific Exception
+    public static class PersonWriterException extends ManageJsonDataCustomException {
 
         // Constructor with message and cause
-        public ManageJsonDataPersonWriterException(String message, Throwable cause) {
-
-            super("error while writing information about a person, additional information:" + message, cause);
+        public PersonWriterException(String message, Throwable cause) {
+            super("Error while writing information about a person, additional information : " + message, cause);
         }
     }
+
+    // Nested static class for fire station writer method to encapsulate throwable general Exception in specific Exception
+    public static class FireStationWriterException extends ManageJsonDataCustomException {
+
+        // Constructor with message and cause
+        public FireStationWriterException(String message, Throwable cause) {
+            super("Error while writing information about a fire station, additional information : " + message, cause);
+        }
+    }
+
+    // Nested static class for medical record writer method to encapsulate throwable general Exception in specific Exception
+    public static class MedicalRecordWriterException extends ManageJsonDataCustomException {
+
+        // Constructor with message and cause
+        public MedicalRecordWriterException(String message, Throwable cause) {
+            super("Error while writing information about a medical record, additional information : " + message, cause);
+        }
+    }
+
+    // Nested static class for init method to encapsulate throwable general Exception in specific Exception
+    public static class InitException extends ManageJsonDataCustomException {
+
+        // Constructor with message and cause
+        public InitException(String message, Throwable cause) {
+            super("error while initialization of ManageJsonData, additional information : " + message, cause);
+        }
+    }
+
 }
