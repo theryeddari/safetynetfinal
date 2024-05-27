3d73f790fa1e2fb4a586d538ceab09a6865ad874
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-27 12:30:00 +0200

    delete message args in custom Exception but not into super class. (message is written in the exception class)

--- src/main/java/com/openclassrooms/safetynet/exceptions/ManageJsonDataCustomException.java
+++ src/main/java/com/openclassrooms/safetynet/exceptions/ManageJsonDataCustomException.java
@@ -2,7 +2,6 @@ package com.openclassrooms.safetynet.exceptions;
 
 // Custom exception class for managing JSON data exceptions
 public class ManageJsonDataCustomException extends Exception {
-
     // Constructor with message and cause
     public ManageJsonDataCustomException(String message, Throwable cause) {
         super(message, cause);
@@ -12,8 +11,8 @@ public class ManageJsonDataCustomException extends Exception {
     public static class PersonWriterException extends ManageJsonDataCustomException {
 
         // Constructor with message and cause
-        public PersonWriterException(String message, Throwable cause) {
-            super("Error while writing information about a person, additional information : " + message, cause);
+        public PersonWriterException(Throwable cause) {
+            super("Error while writing information about a person, additional information : " +cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
 
@@ -21,8 +20,8 @@ public class ManageJsonDataCustomException extends Exception {
     public static class FireStationWriterException extends ManageJsonDataCustomException {
 
         // Constructor with message and cause
-        public FireStationWriterException(String message, Throwable cause) {
-            super("Error while writing information about a fire station, additional information : " + message, cause);
+        public FireStationWriterException(Throwable cause) {
+            super("Error while writing information about a fire station, additional information : " +cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
 
@@ -30,17 +29,16 @@ public class ManageJsonDataCustomException extends Exception {
     public static class MedicalRecordWriterException extends ManageJsonDataCustomException {
 
         // Constructor with message and cause
-        public MedicalRecordWriterException(String message, Throwable cause) {
-            super("Error while writing information about a medical record, additional information : " + message, cause);
+        public MedicalRecordWriterException(Throwable cause) {
+            super("Error while writing information about a medical record, additional information : "+cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
 
     // Nested static class for init method to encapsulate throwable general Exception in specific Exception
     public static class InitException extends ManageJsonDataCustomException {
-
         // Constructor with message and cause
-        public InitException(String message, Throwable cause) {
-            super("error while initialization of ManageJsonData, additional information : " + message, cause);
+        public InitException(Throwable cause) {
+            super("error while initialization of ManageJsonData, additional information : " +cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
 
