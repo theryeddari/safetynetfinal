697af32f46618610f58526f5cdbb10891c46155e
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-28 21:50:18 +0200

    add exceptions and one constructor with just super String

--- src/main/java/com/openclassrooms/safetynet/exceptions/FireStationCustomException.java
+++ src/main/java/com/openclassrooms/safetynet/exceptions/FireStationCustomException.java
@@ -3,25 +3,47 @@ package com.openclassrooms.safetynet.exceptions;
 public class FireStationCustomException extends Exception{
     // Constructor with message and cause
     public FireStationCustomException(String message, Throwable cause) {super(message,cause);}
+    public FireStationCustomException(String message) {super(message);}
 
     // Nested static class for add fire station method to encapsulate throwable general Exception in specific Exception
     public static class AddFireStationException extends FireStationCustomException {
-        public AddFireStationException(String message, Throwable cause) {
-            super("Error while adding a fire station, additional information : " + message, cause);
+        public AddFireStationException(Throwable cause) {
+            super("Error while adding a fire station, additional information : " +cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
 
     // Nested static class for update fire station method to encapsulate throwable general Exception in specific Exception
     public static class UpdateFireStationException extends FireStationCustomException {
-        public UpdateFireStationException(String message, Throwable cause) {
-            super("Error while updating a fire station, additional information : " + message, cause);
+        public UpdateFireStationException(Throwable cause) {
+            super("Error while updating a fire station, additional information : "  +cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
 
-    // Nested static class for update fire station method to encapsulate throwable general Exception in specific Exception
+    // Nested static class for delete fire station method to encapsulate throwable general Exception in specific Exception
     public static class DeleteFireStationException extends FireStationCustomException {
-        public DeleteFireStationException(String message, Throwable cause) {
-            super("Error while deleting a fire station, additional information : " + message, cause);
+        public DeleteFireStationException(Throwable cause) {
+            super("Error while deleting a fire station, additional information : "  +cause.getClass() +" "+ cause.getMessage(), cause);
         }
+    }
+
+    // Nested static class for add fire station method for exception when fireStation already exist
+    public static class AlreadyExistFireStationException extends FireStationCustomException {
+        public AlreadyExistFireStationException() {
+            super("Error, The Station fire file already exists, if you wish to modify it use the update procedure");
+        }
+    }
+
+    public static class NotFoundFireStationException extends FireStationCustomException {
+        public NotFoundFireStationException() {
+            super("Error, The Station fire file wanted its not found, if you wish to add it use the add procedure");
+        }
+    }
+
+    public static class MissingFireStationArgument extends FireStationCustomException{
+
+        public MissingFireStationArgument() {
+            super("Error, address and station number not provided try again");
+        }
+
     }
 }
