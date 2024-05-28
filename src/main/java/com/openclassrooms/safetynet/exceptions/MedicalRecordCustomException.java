41baabaa8394c53e8710bf91e871e0d940edc889
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-28 15:15:21 +0200

    add exceptions and one constructor with just super Throwable

--- src/main/java/com/openclassrooms/safetynet/exceptions/MedicalRecordCustomException.java
+++ src/main/java/com/openclassrooms/safetynet/exceptions/MedicalRecordCustomException.java
@@ -4,25 +4,43 @@ public class MedicalRecordCustomException extends Exception{
     // Constructor with message and cause
     public MedicalRecordCustomException(String message, Throwable cause) {super(message, cause);}
 
+    // Constructor with message
+    public MedicalRecordCustomException(String message) {super(message);}
+
     // Nested static class for add medical record method to encapsulate throwable general Exception in specific Exception
     public static class AddMedicalRecordException extends MedicalRecordCustomException {
-        public AddMedicalRecordException(String message, Throwable cause) {
-            super("Error while adding a medical record, additional information : " + message, cause);
+        public AddMedicalRecordException(Throwable cause) {
+            super("Error while adding a medical record, additional information : " +cause.getClass().getName() +" "+ cause.getMessage(), cause);
         }
     }
 
     // Nested static class for update medical record method to encapsulate throwable general Exception in specific Exception
     public static class UpdateMedicalRecordException extends MedicalRecordCustomException {
-        public UpdateMedicalRecordException(String message, Throwable cause) {
-            super("Error while updating a medical record, additional information : " + message, cause);
+        public UpdateMedicalRecordException(Throwable cause) {
+            super("Error while updating a medical record, additional information : " +cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
 
     // Nested static class for update medical record method to encapsulate throwable general Exception in specific Exception
     public static class DeleteMedicalRecordException extends MedicalRecordCustomException {
-        public DeleteMedicalRecordException(String message, Throwable cause) {
-            super("Error while deleting a medical record, additional information : " + message, cause);
+        public DeleteMedicalRecordException(Throwable cause) {
+            super("Error while deleting a medical record, additional information : " +cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
-    
+
+    // Nested static class for add medical record method for exception when medical record already exist
+    public static class AlreadyExistMedicalRecordException extends MedicalRecordCustomException {
+
+        public AlreadyExistMedicalRecordException() {
+            super("Error, The Medical file already exists, if you wish to modify it use the update procedure");
+        }
+    }
+
+    // Nested static class for update medical record method for exception when medical record not found
+    public static class NotFoundMedicalRecordException extends MedicalRecordCustomException {
+
+        public NotFoundMedicalRecordException() {
+            super("Error, The Medical file wanted its not found, if you wish to add it use the add procedure");
+        }
+    }
 }
