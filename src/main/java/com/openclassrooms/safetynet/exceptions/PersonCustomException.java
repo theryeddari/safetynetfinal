a4483e5e44334e72fee75d0f2abc67f556d397df
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-28 23:15:13 +0200

    add exceptions and one constructor with just super String (person)

--- src/main/java/com/openclassrooms/safetynet/exceptions/PersonCustomException.java
+++ src/main/java/com/openclassrooms/safetynet/exceptions/PersonCustomException.java
@@ -5,25 +5,42 @@ public class PersonCustomException extends Exception {
 
     // Constructor with message and cause
     public PersonCustomException(String message, Throwable cause) {super(message, cause);}
+    public PersonCustomException(String message) {super(message);}
 
     // Nested static class for add person method to encapsulate throwable general Exception in specific Exception
     public static class AddPersonException extends PersonCustomException {
-        public AddPersonException(String message, Throwable cause) {
-            super("Error while adding a person, additional information : " + message, cause);
+        public AddPersonException(Throwable cause) {
+            super("Error while adding a person, additional information : "  +cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
 
     // Nested static class for update person method to encapsulate throwable general Exception in specific Exception
     public static class UpdatePersonException extends PersonCustomException {
-        public UpdatePersonException(String message, Throwable cause) {
-            super("Error while updating a person, additional information : " + message, cause);
+        public UpdatePersonException(Throwable cause) {
+            super("Error while updating a person, additional information : "  +cause.getClass() +" "+ cause.getMessage(), cause);
         }
     }
 
-    // Nested static class for update person method to encapsulate throwable general Exception in specific Exception
+    // Nested static class for delete person method to encapsulate throwable general Exception in specific Exception
     public static class DeletePersonException extends PersonCustomException {
-        public DeletePersonException(String message, Throwable cause) {
-            super("Error while deleting a person, additional information : " + message, cause);
+        public DeletePersonException(Throwable cause) {
+            super("Error while deleting a person, additional information : "  +cause.getClass() +" "+ cause.getMessage(), cause);
+        }
+    }
+
+    // Nested static class for add person method for exception when medical record already exist
+    public static class AlreadyExistPersonException extends PersonCustomException {
+
+        public AlreadyExistPersonException() {
+            super("Error, The person file already exists, if you wish to modify it use the update procedure");
+        }
+    }
+
+    // Nested static class for update person method for exception when medical record not found
+    public static class NotFoundPersonException extends PersonCustomException {
+
+        public NotFoundPersonException() {
+            super("Error, The person file wanted its not found, if you wish to add it use the add procedure");
         }
     }
 
