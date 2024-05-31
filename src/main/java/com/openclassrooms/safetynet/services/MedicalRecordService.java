48ffe550e524cdbd73f055dae81a50c60c54a68f
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-05-31 18:27:25 +0200

    add log

--- src/main/java/com/openclassrooms/safetynet/services/MedicalRecordService.java
+++ src/main/java/com/openclassrooms/safetynet/services/MedicalRecordService.java
@@ -12,8 +12,12 @@ import java.util.Optional;
 
 import static com.openclassrooms.safetynet.exceptions.MedicalRecordCustomException.*;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @Service
 public class MedicalRecordService {
+    private static final Logger logger = LogManager.getLogger(MedicalRecordService.class);
     private final ManageJsonData manageJsonData;
 
     // Constructor to inject ManageJsonData dependency
@@ -23,52 +27,89 @@ public class MedicalRecordService {
 
     // Method to add a new medical record
     public void addMedicalRecord(AddMedicalRecordDto newMedicalRecord) throws AddMedicalRecordException {
-       try{
-        List<MedicalRecordModel> listMedicalRecordExisting = manageJsonData.medicalRecordReaderJsonData();
-        // Check if the medical record already exists
-        if (listMedicalRecordExisting.stream().noneMatch(medicalRecordExist -> medicalRecordExist.getLastName().equals(newMedicalRecord.getLastName()) && medicalRecordExist.getFirstName().equals(newMedicalRecord.getFirstName()))) {
-            // Add the new medical record if it does not exist
-            listMedicalRecordExisting.add(new MedicalRecordModel(newMedicalRecord.getFirstName(), newMedicalRecord.getLastName(), newMedicalRecord.getBirthdate(), newMedicalRecord.getMedications(), newMedicalRecord.getAllergies()));
-        }else {throw new AlreadyExistMedicalRecordException();}
-        // Write the updated list of medical records back to the JSON file
-        manageJsonData.medicalRecordWriterJsonData(listMedicalRecordExisting);
-       }catch(Exception e){
-           throw new AddMedicalRecordException(e);
+        logger.info("Adding new medical record for {} {}", newMedicalRecord.getFirstName(), newMedicalRecord.getLastName());
+        try {
+            List<MedicalRecordModel> listMedicalRecordExisting = manageJsonData.medicalRecordReaderJsonData();
+            logger.debug("Existing medical records: {}", listMedicalRecordExisting);
+
+            // Check if the medical record already exists
+            if (listMedicalRecordExisting.stream().noneMatch(medicalRecordExist ->
+                    medicalRecordExist.getLastName().equals(newMedicalRecord.getLastName()) &&
+                            medicalRecordExist.getFirstName().equals(newMedicalRecord.getFirstName()))) {
+
+                // Add the new medical record if it does not exist
+                listMedicalRecordExisting.add(new MedicalRecordModel(
+                        newMedicalRecord.getFirstName(),
+                        newMedicalRecord.getLastName(),
+                        newMedicalRecord.getBirthdate(),
+                        newMedicalRecord.getMedications(),
+                        newMedicalRecord.getAllergies()));
+                logger.info("New medical record added successfully.");
+            } else {
+                throw new AlreadyExistMedicalRecordException();
+            }
+
+            // Write the updated list of medical records back to the JSON file
+            manageJsonData.medicalRecordWriterJsonData(listMedicalRecordExisting);
+        } catch (Exception e) {
+            logger.error("Error adding medical record: ", e);
+            throw new AddMedicalRecordException(e);
         }
     }
 
     // Method to update an existing medical record
     public void updateMedicalRecord(UpdateMedicalRecordDto updateMedicalRecord) throws UpdateMedicalRecordException {
+        logger.info("Updating medical record for {} {}", updateMedicalRecord.getFirstName(), updateMedicalRecord.getLastName());
         try {
             List<MedicalRecordModel> listMedicalRecordExisting = manageJsonData.medicalRecordReaderJsonData();
+            logger.debug("Existing medical records: {}", listMedicalRecordExisting);
+
             // Get the reference to the filtered medical record object
-           Optional<MedicalRecordModel> wantedMedicalRecordUpdate = listMedicalRecordExisting.stream().filter(medicalRecord -> medicalRecord.getFirstName().equals(updateMedicalRecord.getFirstName()) && medicalRecord.getLastName().equals(updateMedicalRecord.getLastName())).findFirst();
-           if(wantedMedicalRecordUpdate.isEmpty()){throw new NotFoundMedicalRecordException();}
-           // If there is a reference, access the object and modify its properties
-                wantedMedicalRecordUpdate.ifPresent(modifyMedicalRecord -> {
+            Optional<MedicalRecordModel> wantedMedicalRecordUpdate = listMedicalRecordExisting.stream()
+                    .filter(medicalRecord -> medicalRecord.getFirstName().equals(updateMedicalRecord.getFirstName()) &&
+                            medicalRecord.getLastName().equals(updateMedicalRecord.getLastName()))
+                    .findFirst();
+
+            if (wantedMedicalRecordUpdate.isEmpty()) {
+                throw new NotFoundMedicalRecordException();
+            }
+
+            // If there is a reference, access the object and modify its properties
+            wantedMedicalRecordUpdate.ifPresent(modifyMedicalRecord -> {
                 modifyMedicalRecord.setBirthdate(updateMedicalRecord.getBirthdate());
                 modifyMedicalRecord.setMedications(updateMedicalRecord.getMedications());
                 modifyMedicalRecord.setAllergies(updateMedicalRecord.getAllergies());
             });
+
             // Write the updated list of medical records back to the JSON file
             manageJsonData.medicalRecordWriterJsonData(listMedicalRecordExisting);
-        } catch(Exception e) {
+            logger.info("Medical record updated successfully.");
+        } catch (Exception e) {
+            logger.error("Error updating medical record: ", e);
             throw new UpdateMedicalRecordException(e);
         }
     }
 
     // Method to delete an existing medical record
     public void deleteMedicalRecord(DeleteMedicalRecordDto deleteMedicalRecord) throws DeleteMedicalRecordException {
+        logger.info("Deleting medical record for {} {}", deleteMedicalRecord.getFirstName(), deleteMedicalRecord.getLastName());
         try {
             List<MedicalRecordModel> listMedicalRecordExisting = manageJsonData.medicalRecordReaderJsonData();
+            logger.debug("Existing medical records: {}", listMedicalRecordExisting);
+
             // Remove the medical record if it matches the given first name and last name
-            if (listMedicalRecordExisting.removeIf(medicalRecord -> medicalRecord.getFirstName().equals(deleteMedicalRecord.getFirstName()) && medicalRecord.getLastName().equals(deleteMedicalRecord.getLastName()))) {
+            if (listMedicalRecordExisting.removeIf(medicalRecord ->
+                medicalRecord.getFirstName().equals(deleteMedicalRecord.getFirstName()) &&
+                medicalRecord.getLastName().equals(deleteMedicalRecord.getLastName()))) {
+
                 // Write the updated list of medical records back to the JSON file
                 manageJsonData.medicalRecordWriterJsonData(listMedicalRecordExisting);
+                logger.info("Medical record deleted successfully.");
             } else {
                 throw new NotFoundMedicalRecordException();
             }
         } catch (Exception e) {
+            logger.error("Error deleting medical record: ", e);
             throw new DeleteMedicalRecordException(e);
         }
     }
